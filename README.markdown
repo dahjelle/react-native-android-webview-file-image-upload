75e045bbe9987d0f4f84768eb3b12232fbb1dcd9
David Alan Hjelle <dahjelle+github.com@thehjellejar.com>
2017-11-17 16:46:32 -0600

    Implement webview file upload on Android.

---
+++ README.markdown
@@ -0,0 +1,13 @@
+# React Native Android Image File Upload Webview (camera + file)
+
+This is a demo project based off of the work of several others (such as [this webview file upload sample code](https://github.com/hushicai/ReactNativeAndroidWebView) and [this sample about overriding the built-in webview](https://github.com/cbrevik/webview-native-config-example)) to implement file upload in the React Native Android WebView.
+
+Ideally all the changes to get it to work (from a basic RN project) should be in a single commit that you can put in your app.)
+
+My particular use-case is focused on pictures, so I limited it to uploading from the camera and images already stored on the device. General file upload shouldn't be significantly different.
+
+It only works with React Native 0.50+, and reverts to the built-in WebView on iOS.
+
+I'm a newbie at Java, so I expect the code could use some improvement. :-D
+
+**NOTE**: At least one version of the Android simulator doesn't seem to do the file upload correctly (it gives a checkered background with a red box instead of showing the file you uploaded), but it appears to work fine on the device. If you know what's going on there, I'd love to hear!
--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text} from 'react-native';
+import {AppRegistry, Stylesheet} from 'react-native';
+import CustomWebView from './CustomWebView';
+
+const HTML = `
+<html>
+<head>
+    <body>
+        <input type="file" name="uploadFile" />
+        <h3></h3>
+        <div style="width: 100%; height: 100%">
+        </div>
+    </body>
+    <script>
+      async function blobToDataURL(image) {
+        return new Promise(function(resolve, reject) {
+          var fileReader = new FileReader();
+          fileReader.onloadend = function() {
+            return resolve(fileReader.result);
+          };
+          fileReader.readAsDataURL(image);
+        });
+      };
+      document.querySelector('input').addEventListener('change', async function(event) {
+        file = event.target.files[0];
+        document.querySelector('div').style.backgroundImage = 'url(' + await blobToDataURL(file) + ')';
+        document.querySelector('h3').innerHTML = file.name + ': ' + file.type + ':' + file.size;
+        console.log(file);
+      });
+    </script>
+</html>
+`;
 
 class App extends Component {
   render() {
-    return <Text>Hello, world!</Text>
+    return <CustomWebView source={{html: HTML}} />;
   }
 }
 
